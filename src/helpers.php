34d9bd369b9a8e799995a46270c36645ca4445dd
Gabriel Peixoto <gabriel@escavador.com>
2026-01-05 20:03:44 -0300

    Update helpers.php
    Permitir passar o valor da tag

--- src/helpers.php
+++ src/helpers.php
@@ -40,11 +40,13 @@ if (!function_exists('array_xml_get')) {
      * @param $key
      * @return null
      */
-    function array_xml_get($array, $key)
+    function array_xml_get($array, $key, $tagName = null)
     {
         if ($value = array_get($array, $key)) {
-            $xmlTag = "<" . studly_case($key) . ">";
-            $xmlCloseTag = "</" . studly_case($key) . ">";
+            $tagName = $tagName ? $tagName : studly_case($key);
+
+            $xmlTag = "<" . $tagName . ">";
+            $xmlCloseTag = "</" . $tagName . ">";
 
             return $xmlTag . $value . $xmlCloseTag;
         }
